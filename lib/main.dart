2352d06297267c7acf0a23296df7208ec93256e0
Tomic, Vladimir <vladimir.tomic@zuehlke.com>
2021-08-06 13:08:30 +0200

    Introduce MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:provider/provider.dart';
 import 'package:todo_tracker/app/about/about_screen.dart';
 import 'package:todo_tracker/app/home/home_screen.dart';
 import 'package:todo_tracker/app/home/home_view_model.dart';
+import 'package:todo_tracker/shared/theme_manager.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(create: (context) => HomeViewModel(),child: MyApp()));
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<HomeViewModel>(create: (_) => HomeViewModel()),
+        Provider<ThemeManager>(create: (_) => ThemeManager()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
