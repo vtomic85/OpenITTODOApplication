6dcdc445a216d86e09f72a836778ecd1e3a6291c
Dushan Milosevic <dusan.milosevic@zuehlke.com>
2022-06-06 12:27:41 +0200

    add sembast_web

--- new_app/lib/data/todo_item/database_connection.dart
+++ new_app/lib/data/todo_item/database_connection.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sembast/sembast.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
 
 class DatabaseConnection {
   Database? _database;
@@ -10,11 +14,17 @@ class DatabaseConnection {
   final int _databaseVersion = 1;
 
   Future<Database> get db async {
+    var directory;
     if (_database == null) {
-      final directory = await getApplicationSupportDirectory();
-      final path = join(directory.path, _databaseName);
-      _database =
-      await databaseFactoryIo.openDatabase(path, version: _databaseVersion);
+      if (kIsWeb) {
+        _database = await databaseFactoryWeb.openDatabase(_databaseName,
+            version: _databaseVersion);
+      } else {
+        directory = await getApplicationSupportDirectory();
+        final path = join(directory.path, _databaseName);
+        _database = await databaseFactoryIo.openDatabase(path,
+            version: _databaseVersion);
+      }
     }
     return _database!;
   }
